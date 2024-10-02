090b1a95118c7c949a1715addba79d57ecce5e01
SecurityQQ <alexmsecurity@gmail.com>
2024-10-02 11:38:49 +0400

    fixed paddings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -131,7 +131,7 @@ export default function BrogevityLanding() {
         </section>
 
         {/* Pain Points Section */}
-        <section className="py-16 bg-background">
+        <section className="py-16 bg-background px-4">
           <div className="container mx-auto px-4">
             <h2 className="text-3xl font-bold text-center mb-8">
               Just Listening To Longevity Podcasts is Overwhelming Because...

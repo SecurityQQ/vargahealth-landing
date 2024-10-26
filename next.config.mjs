a3b44ffe56e6a1908890a7c81152440cb4feab2e
SecurityQQ <alexmsecurity@gmail.com>
2024-10-26 18:59:40 +0400

    add redirect to interview

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/interview',
+        destination: 'https://calendly.com/annzrva108/15min',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
